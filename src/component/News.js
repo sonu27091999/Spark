b2acc7527f0775380c1fc9f9980caba77f53cbf1
papakagithub <shah.27091999@gmial.com>
2022-05-10 11:52:00 +0530

    Adding previous & Next buttons to populate NewItems

--- src/component/News.js
+++ src/component/News.js
@@ -7,21 +7,50 @@ export class News extends Component {
         console.log('Hello i am a constructor from news component');
         this.state = {
             articles: [],
-            loading: false
+            loading: false,
+            page: 1,
+            totalResults: 0
         }
     }
-    async componentDidMount(){
+    async componentDidMount() {
         console.log('cdm');
-        let url='https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=e27d749b9d614d81b3ac43aab784185f';
-        let data=await fetch(url);
-        let parseData= await data.json();
+        let url = 'https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=e27d749b9d614d81b3ac43aab784185f&page=1&pageSize=20';
+        let data = await fetch(url);
+        let parseData = await data.json();
         console.log(parseData);
-        this.setState({articles: parseData.articles});
+        this.setState({ articles: parseData.articles, totalResults: parseData.totalResults });
+    }
+    handlePreviousClick = async () => {
+        console.log('previous');
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=e27d749b9d614d81b3ac43aab784185f&page=${this.state.page - 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parseData = await data.json();
+        console.log(parseData);
+        this.setState({
+            page: this.state.page - 1,
+            articles: parseData.articles
+        })
+    }
+    handleNextClick = async () => {
+        console.log('next');
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+        } else {
+            let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=e27d749b9d614d81b3ac43aab784185f&page=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parseData = await data.json();
+            console.log(parseData);
+            this.setState({
+                page: this.state.page + 1,
+                articles: parseData.articles
+            })
+        }
     }
 
     render() {
         console.log('render');
         return (
+
             <div className='container my-3'>
                 <h2>NewsMonkey - Top headlines</h2>
                 <div className="row my-3">
@@ -30,6 +59,10 @@ export class News extends Component {
                             <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newUrl={element.url} />
                         </div>
                     })};
+                </div>
+                <div className="container my-3 d-flex justify-content-between">
+                    <button type="button" disabled={this.state.page <= 1} rel="noreferrer" className="btn btn-dark" onClick={this.handlePreviousClick}>&larr;Previous</button>
+                    <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 20)} rel="noreferrer" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
                 </div>
             </div>
         )
