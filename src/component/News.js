fb41870fb70182fead19346c7cc8cee6b176f729
papakagithub <shah.27091999@gmial.com>
2022-05-11 10:13:31 +0530

    Adding time, author and news source

--- src/component/News.js
+++ src/component/News.js
@@ -75,7 +75,7 @@ export class News extends Component {
                 <div className="row my-3">
                     {!this.state.loading && this.state.articles.map((element) => {
                         return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newUrl={element.url} />
+                            <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
                         </div>
                     })};
                 </div>
--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let { title, description, imageUrl, newUrl } = this.props;
+        let { title, description, imageUrl, newUrl, author, date, source } = this.props;
         return (
             <div>
-                <div className="card my-1">
+                <div className="card my-3">
+                    <span style={{ zIndex: '1', left: '91%' }} className="position-absolute top-0 translate-middle badge rounded-pill bg-danger">
+                        {source}
+                    </span>
                     <img src={imageUrl ? imageUrl : 'https://images.moneycontrol.com/static-mcnews/2022/02/LIC-IPO-770x433.jpg'} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {author ? author : 'unknown'} on {new Date(date).toGMTString()}</small></p>
                         <a href={newUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
